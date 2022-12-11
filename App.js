fe6aa677ff2315028f4b26e52e5581b42cb458a1
mimino7 <mimino7@mail.ru>
2022-12-11 15:36:51 +0300

    first_Authscreen_enter_login_splash

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import Svg, { Image } from "react-native-svg";
+import styles from "./styles";
 
 export default function App() {
+  const { width, height } = Dimensions.get("window");
+
+  console.log(width);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={StyleSheet.absoluteFill}>
+        {/* StyleSheet.absoluteFill - зто то же самое что и style={position: 'absolute', left: 0, right: 0, top: 0, bottom: 0} */}
+        <Svg width={width + 1} height={height}>
+          <Image
+            href={require("./assets/splash_2.jpg")}
+            width={width}
+            height={height}
+            preserveAspectRatio="xMidYMid slice" // на весь экран
+          />
+        </Svg>
+      </View>
+
+      <View style={styles.button_container}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>LOG IN</Text>
+        </View>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>REGISTR</Text>
+        </View>
+      </View>
+
+      <StatusBar hidden style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,85 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  button: {
+    height: 55,
+    backgroundColor: "#416A80",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 35,
+    marginHorizontal: 28,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "white",
+  },
+  formButton: {
+    height: 55,
+    backgroundColor: "#416A80",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 35,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "white",
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "600",
+    letterSpacing: 0.5,
+  },
+  button_container: {
+    justifyContent: "center",
+    height: height / 3,
+  },
+  formInputContainer: {
+    marginBottom: 70,
+  },
+  closeButtonContainer: {
+    justifyContent: "center",
+    alignSelf: "center",
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    alignItems: "center",
+    backgroundColor: "white",
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+
+    elevation: 5,
+  },
+  textInput: {
+    height: 50,
+    borderColor: "grey",
+    borderWidth: 1,
+    borderRadius: 25,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    paddingLeft: 10,
+  },
+});
+
+export default styles;
